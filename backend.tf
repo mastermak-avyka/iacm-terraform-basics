c4ee4bfc9e4218b07f70f8ed92291dc47e4373ad
mastermak-avyka <attaullah.n.khan@avyka.com>
2026-07-01 05:47:33 +0000

    backend updated

--- backend.tf
+++ backend.tf
@@ -1,14 +1,22 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state"
-    key          = "iacm/poc1/terraform.tfstate"
-    region       = "us-east-1"
-    access_key   = "test"
-    secret_key   = "test"
-    endpoint     = "http://localhost:4566"
-    use_path_style = true
+    bucket                      = "terraform-state"
+    key                         = "iacm/poc1/terraform.tfstate"
+    region                      = "us-east-1"
+    access_key                  = "test"
+    secret_key                  = "test"
+    
+    # 1. Route traffic out of the Docker container to your host machine
+    endpoint                    = "http://host.docker.internal:4566"
+    
+    # 2. Force path-style addressing (both arguments included to cover all TF versions)
+    use_path_style              = true
+    force_path_style            = true 
+    
+    # 3. Skip AWS validations
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
   }
 }
